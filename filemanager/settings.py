97d484c8cd802eac13616daa55916544853d80f2
Curtis <curtis@tinbrain.net>
2015-12-22 01:58:38 +1100

    Tidy with flake8. Make Py3/Dj1.9 functional.

--- filemanager/settings.py
+++ filemanager/settings.py
@@ -2,10 +2,8 @@ import os
 from django.conf import settings
 
 FILEMANAGER_STATIC_ROOT = getattr(settings, 'FILEMANAGER_STATIC_ROOT',
-      os.path.dirname(os.path.abspath(__file__))+'/static/filemanager/')
+                                  os.path.dirname(os.path.abspath(__file__))+'/static/filemanager/')
 FILEMANAGER_CKEDITOR_JS = getattr(settings, 'FILEMANAGER_CKEDITOR_JS',
-                            'ckeditor/ckeditor.js')
-FILEMANAGER_CHECK_SPACE = getattr(settings, 'FILEMANAGER_CHECK_SPACE',
-                            True)
-FILEMANAGER_SHOW_SPACE = getattr(settings, 'FILEMANAGER_SHOW_SPACE',
-                            FILEMANAGER_CHECK_SPACE)
+                                  'ckeditor/ckeditor.js')
+FILEMANAGER_CHECK_SPACE = getattr(settings, 'FILEMANAGER_CHECK_SPACE', True)
+FILEMANAGER_SHOW_SPACE = getattr(settings, 'FILEMANAGER_SHOW_SPACE', FILEMANAGER_CHECK_SPACE)
