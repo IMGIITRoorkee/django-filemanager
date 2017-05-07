330c4bb0c6df1136dbc5cac11d93b43422dbb395
Rohith Asrk <rohith.asrk@gmail.com>
2017-05-08 00:12:59 +0530

    Added all files necessary for Travis CI to perform tests

--- setup.py
+++ setup.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
-from pip.req import parse_requirements
+import os
+import sys
 
-# parse requirements
-# reqs = parse_requirements("requirements/common.txt")
+from setuptools import find_packages, setup
 
-# setup the project
+
+def get_install_requires():
+    """
+    parse requirements.txt, ignore links, exclude comments
+    """
+    requirements = []
+    for line in open('requirements.txt').readlines():
+        # skip to next iteration if comment or empty line
+        if line.startswith('#') or line == '' or line.startswith('http') or line.startswith('git'):
+            continue
+        # add line to requirements
+        requirements.append(line)
+    return requirements
+
+
 setup(
     name="django-filemanager",
     version="0.0.1",
     author="Information Management Group",
     author_email="img.iitr.img@gmail.com",
     description="A file manager for Django",
-    license="IMG",
+    license="MIT",
     packages=find_packages(exclude=["tests", ]),
-    # install_requires=[str(x).split(' ')[0] for x in reqs],
+    install_requires=get_install_requires(),
     zip_safe=False,
     include_package_data=True,
     test_suite='tests.main',
