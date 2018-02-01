4186728f99890bdd182b420b2a6e6b82ecdfb691
Rohith Asrk <rohith.asrk@gmail.com>
2018-02-01 21:19:05 +0530

    Added example allowed extensions to test project

--- tests/views.py
+++ tests/views.py
@@ -3,5 +3,6 @@ from settings import MEDIA_ROOT
 
 
 def view(request, path):
-    fm = FileManager(MEDIA_ROOT)
+    extensions = ['html', 'htm', 'zip', 'py', 'css', 'js', 'jpeg', 'jpg', 'png']
+    fm = FileManager(MEDIA_ROOT, extensions=extensions)
     return fm.render(request, path)
